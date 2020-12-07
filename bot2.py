69e163945509e3ac77e6efe661d48e227947749c
Boombap2 <boombap2@Boombap2s.local>
2020-12-07 08:51:27 -0800

    testing the new git process from local

--- bot2.py
+++ bot2.py
@@ -2,6 +2,11 @@ from selenium import webdriver
 from time import sleep
 from sauce import pw
 
+#based upon this youtube tutorial: https://www.youtube.com/watch?v=7qcQDeShXpg
+#get errors for the following: aise exception_class(message, screen, stacktrace)
+#selenium.common.exceptions.NoSuchElementException: Message: no such element: Unable to locate element: {"method":"xpath","selector":"//a[contains(text(), 'Log in')]"}
+# (Session info: chrome=87.0.4280.67)
+
 class GoBot:
     def __init__(self,username,pw):
         self.driver = webdriver.Chrome('./chromedriver')
